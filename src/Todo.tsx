6b87fe25541aa0402e644f5df2f5e0e50188a075
ishikawa-kazuhiko <85629860+ishikawa-kazuhiko@users.noreply.github.com>
2021-06-14 18:56:29 +0900

    omitとpick omitとpick

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,10 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+import { TodoType } from "./types/todo";
+//ここのTodoTypeは、idが不要
 
-export const Todo = (props: TodoType) => {
+export const Todo = (
+  // props: Pick<TodoType, "userId" | "title" | "completed"> //必要な変数のみ取得する書き方
+  props: Omit<TodoType, "id"> //idを除いた場合の書き方
+) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
